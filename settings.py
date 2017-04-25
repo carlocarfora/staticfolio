605bef02e679a8587fa63c5e66faa2d53a93a41e
carlo <carlo@carlocarfora.co.uk>
2017-04-26 00:45:48 +0800

    Styling and content left. Refactor to publish on master and update readme

--- settings.py
+++ settings.py
@@ -10,10 +10,11 @@ tags = {
         "(%_vimeo_url_%)": "http://www.vimeo.com/carlocarfora",
         "(%_twitter_url_%)": "http://www.twitter.com/carlo_carfora",
         "(%_flickr_url_%)": "http://www.flickr.com/carlo_c",
+        "(%_github_url_%)": "https://github.com/carlocarfora",
         "(%_logo_%)": '''<img src="images/avatar.jpg" alt="" />''',
         "(%_tagline_%)": "3D/FX Artist",
         "(%_twitter_code_%)": '''<a class="twitter-timeline" data-chrome="noheader noborders transparent" height="300" data-dnt="true" href="https://twitter.com/carlo_carfora" data-widget-id="349595905890086912" data-tweet-limit="4">Tweets by @carlo_carfora</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>''',
-        "(%_footer_%)": "© 2016 Carlo Carfora – Built with Staticfolio."
+        "(%_footer_%)": "© 2017 Carlo Carfora – Built with Staticfolio. Design from templated.co"
         }
         
 links = {
